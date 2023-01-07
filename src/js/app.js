60bd459de9f4fcb1ca3e836bdf0e45e3cd3043bf
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-01-07 19:23:40 +0100

    function to open and close menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log("hi app")
+const menuBox = document.querySelector(".menu-close");
+const menuItems = document.querySelectorAll(".menu__list--text");
+const menuBtn = document.querySelector(".menu__title");
+
+const openMenu = () => {
+  menuBox.classList.toggle("menu");
+
+  if (menuBox.classList.contains("menu-close")) {
+    menuBox.classList.remove("menu-close");
+  } else {
+    menuBox.classList.add("menu-close");
+  }
+
+  menuItems.forEach((item) => {
+    if (item.classList.contains("hidden")) {
+      item.classList.remove("hidden");
+    } else {
+      item.classList.add("hidden");
+    }
+  });
+};
+
+menuBtn.addEventListener("click", openMenu);
