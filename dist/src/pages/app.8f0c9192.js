7df72749fd1cf08893b10e308acfea29a6bf2ba3
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-01-09 20:08:24 +0100

    function to add new task

--- dist/src/pages/app.8f0c9192.js
+++ dist/src/pages/app.8f0c9192.js
@@ -66,5 +66,45 @@ cardBtn.forEach((btn)=>{
         }
     });
 });
+// TASK SECTION
+const taskInput = document.querySelector(".tasks__input");
+const taskBox = document.querySelector(".new-tasks-box");
+const addTaskBtn = document.querySelector(".tasks__add-btn");
+let taskPlace;
+let newTask;
+const addNewTask = ()=>{
+    if (taskInput.value !== "") {
+        taskPlace = document.createElement("div");
+        taskPlace.classList.add("tasks__new-task");
+        taskBox.appendChild(taskPlace);
+        newTask = document.createElement("p");
+        newTask.innerHTML = taskInput.value;
+        newTask.classList.add("tasks__new-task--text");
+        taskPlace.appendChild(newTask);
+        createNewTaskBtns();
+    } else console.log("wprowadz tekst");
+    taskInput.value = "";
+};
+const createNewTaskBtns = ()=>{
+    const btnsArea = document.createElement("div");
+    btnsArea.classList.add("tasks__new-task--buttons");
+    taskPlace.appendChild(btnsArea);
+    const acceptBtn = document.createElement("button");
+    acceptBtn.classList.add("tasks__new-task--accept-btn");
+    acceptBtn.classList.add("tasks__new-task--btn");
+    acceptBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("tasks__new-task--edit-btn");
+    editBtn.classList.add("tasks__new-task--btn");
+    editBtn.innerHTML = "edit";
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("tasks__new-task--trash-btn");
+    deleteBtn.classList.add("tasks__new-task--btn");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    btnsArea.appendChild(acceptBtn);
+    btnsArea.appendChild(editBtn);
+    btnsArea.appendChild(deleteBtn);
+};
+addTaskBtn.addEventListener("click", addNewTask);
 
 //# sourceMappingURL=app.8f0c9192.js.map
