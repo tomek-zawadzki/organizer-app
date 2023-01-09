d65eaade1d1065d43dd7ca2af490de1a8e8cca84
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-01-09 16:42:31 +0100

    implementing change cards after click button in menu

--- dist/src/pages/app.8f0c9192.js
+++ dist/src/pages/app.8f0c9192.js
@@ -32,12 +32,39 @@ const getQuote = async ()=>{
 getQuote();
 changeQuoteBtn.addEventListener("click", getQuote);
 // CHANGING MENU CARDS
-const menuCardsBtn = document.querySelectorAll(".menu__list--item");
 const cardBtn = document.querySelectorAll(".menu__list--btn");
-console.log(menuCardsBtn);
-console.log(cardBtn);
+const cardNotes = document.querySelector(".notes");
+const cardWallet = document.querySelector(".wallet");
+const cardCalendar = document.querySelector(".calendar");
+const cardTasks = document.querySelector(".tasks");
+const cards = document.querySelectorAll(".card");
 cardBtn.forEach((btn)=>{
-    btn.addEventListener("click", console.log("ok"));
+    btn.addEventListener("click", (e)=>{
+        if (e.target.closest("li").classList.contains("item-tasks")) {
+            cards.forEach((card)=>card.classList.remove("active-card"));
+            cards.forEach((card)=>card.classList.add("hidden"));
+            cardTasks.classList.remove("hidden");
+            cardTasks.classList.add("active-card");
+        }
+        if (e.target.closest("li").classList.contains("item-notes")) {
+            cards.forEach((card)=>card.classList.remove("active-card"));
+            cards.forEach((card)=>card.classList.add("hidden"));
+            cardNotes.classList.remove("hidden");
+            cardNotes.classList.add("active-card");
+        }
+        if (e.target.closest("li").classList.contains("item-wallet")) {
+            cards.forEach((card)=>card.classList.remove("active-card"));
+            cards.forEach((card)=>card.classList.add("hidden"));
+            cardWallet.classList.remove("hidden");
+            cardWallet.classList.add("active-card");
+        }
+        if (e.target.closest("li").classList.contains("item-calendar")) {
+            cards.forEach((card)=>card.classList.remove("active-card"));
+            cards.forEach((card)=>card.classList.add("hidden"));
+            cardCalendar.classList.remove("hidden");
+            cardCalendar.classList.add("active-card");
+        }
+    });
 });
 
 //# sourceMappingURL=app.8f0c9192.js.map
