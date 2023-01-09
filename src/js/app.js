a83c4ebf91afb69d28b627c9a6c9856ed79a08fb
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-01-09 14:41:54 +0100

    implementing quotes generator

--- src/js/app.js
+++ src/js/app.js
@@ -21,3 +21,39 @@ const openMenu = () => {
 };
 
 menuBtn.addEventListener("click", openMenu);
+
+// QUOTE GENERATOR
+const quoteText = document.querySelector(".quotes__text");
+const quoteAuthor = document.querySelector(".quotes__author");
+const changeQuoteBtn = document.querySelector(".quotes__change-btn");
+
+const changeQuote = (data) => {
+  const random = Math.floor(Math.random() * data.length);
+  quoteText.innerHTML = data[random].text;
+  quoteAuthor.innerHTML = data[random].author;
+};
+
+const getQuote = async () => {
+  try {
+    const res = await fetch("https://type.fit/api/quotes");
+    const json = await res.json();
+    changeQuote(json);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getQuote();
+
+changeQuoteBtn.addEventListener("click", getQuote);
+
+// CHANGING MENU CARDS
+const menuCardsBtn = document.querySelectorAll(".menu__list--item");
+const cardBtn = document.querySelectorAll(".menu__list--btn");
+
+console.log(menuCardsBtn);
+console.log(cardBtn);
+
+cardBtn.forEach((btn) => {
+  btn.addEventListener("click", console.log("ok"));
+});
